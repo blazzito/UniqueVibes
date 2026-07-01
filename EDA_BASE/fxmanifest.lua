2c65f449e7cbd17fbed16737795ce2dbf3f5b778
cristian aguirre <cristian.aguirre@quanticsgroup.com>
2026-06-30 23:36:53 -0500

    drive by

---
+++ EDA_BASE/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Drive By Mechanic - Base Script'
+
+client_scripts {
+    'config.lua',
+    'driveby.lua'
+}
--- test.lua
+++ test.lua
@@ -1,16 +1,20 @@
-local json = require('json') -- If not available, we just test basic structure
+local currentDate = "2026-06-28"
+local lastDate = "2026-06-27"
+local streak = 1
 
-local options = {}
-for i = -1, 5 do
-    table.insert(options, {
-        label = 'Test',
-        img = 'img/icons/respray.png',
-        price = 0,
-        priceMult = 0.0
-    })
+local y, m, d = currentDate:match("(%d+)-(%d+)-(%d+)")
+local todayTime = os.time({year=y, month=m, day=d, hour=12})
+
+local isYesterday = false
+local ly, lm, ld = lastDate:match("(%d+)-(%d+)-(%d+)")
+local lastTime = os.time({year=ly, month=lm, day=ld, hour=12})
+
+print("todayTime:", todayTime)
+print("lastTime:", lastTime)
+print("Diff:", todayTime - lastTime)
+
+if (todayTime - lastTime <= 86400 * 1.5) and (todayTime - lastTime >= 86400 * 0.5) then
+    isYesterday = true
 end
 
-print('Length:', #options)
-for k,v in pairs(options) do
-    print(k, v.label)
-end
+print("isYesterday:", isYesterday)
